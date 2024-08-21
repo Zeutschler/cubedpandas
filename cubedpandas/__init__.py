c692b93f759d4fe24a4985e96bf1778a5b35c090
Thomas Zeutschler <info@tinyolap.com>
2024-08-21 13:37:03 +0200

    license changed

--- cubedpandas/__init__.py
+++ cubedpandas/__init__.py
@@ -26,7 +26,7 @@ from cubedpandas.context.member_not_found_context import MemberNotFoundContext
 
 from cubedpandas.slice.slice import Slice
 
-__version__ = "0.2.14"
+__version__ = "0.2.15"
 VERSION = __version__
 
 __all__ = [
