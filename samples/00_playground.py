88e2f8ab8b647eced844a6c481fac33e238b221a
Thomas Zeutschler <thomas.zeutschler@gmail.com>
2024-09-22 21:10:02 +0200

    DateSpan & DateFilter carved out to separate project 'DateSpanLib' https://github.com/Zeutschler/DateSpanLib

--- samples/00_playground.py
+++ samples/00_playground.py
@@ -12,11 +12,4 @@ cdf = cubed(df)
 
 cdf.settings.debug_mode = True
 context = cdf.product["Apple", "Pear"].sales
-print(context.address)
-
-print(context.dsf_asdfsd)
-
-value = df.loc[(df['make'] == 'Audi') &
-               (df['engine'] == 'hybrid') &
-               (df['date'] >= '2024-09-01') & (df['date'] <= '2024-09-30'),
-'price'].sum()
+print(f"{context.address} = {context}")
